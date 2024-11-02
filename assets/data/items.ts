36cb5e7ab672b8e0389de45852d400dbe097f697
🅿️adi <148848073+lukepadiachy@users.noreply.github.com>
2024-11-02 23:16:52 +0200

    Fixed up designs
    - fixed up my data page for items
    - removed stocktake page
    - pulled info from items to index screen

--- assets/data/items.ts
+++ assets/data/items.ts
@@ -1,37 +1,105 @@
 
-export type Category = 'Fridge' | 'Cupboard' | 'Pantry' | 'Bathroom' | 'Bedroom' | 'Living Room';
+const items = [
+  {
+    id: '1',
+    name: 'Rooibos Tea',
+    quantity: 2,
+    category: 'Cupboard',
+    addedAt: new Date('2023-10-01'),
+    updatedAt: new Date('2023-10-01'),
+    expiryDate: new Date('2024-10-01'),
+    image: 'https://m.faithful-to-nature.co.za/media/catalog/product/cache/1/image/400x400/9df78eab33525d08d6e5fb8d27136e95/c/a/carmien_carmien_rooibos_relax_sku123472.jpg',
+  },
+  {
+    id: '2',
+    name: 'Maize Meal',
+    quantity: 1,
+    category: 'Cupboard',
+    addedAt: new Date('2023-10-01'),
+    updatedAt: new Date('2023-10-01'),
+    expiryDate: new Date('2024-04-01'),
+    image: 'https://rclfoods.com/wp-content/uploads/2021/04/Tafelberg-125kg.jpg',
+  },
+  {
+    id: '3',
+    name: 'Baked Beans',
+    quantity: 4,
+    category: 'Cupboard',
+    addedAt: new Date('2023-10-01'),
+    updatedAt: new Date('2023-10-01'),
+    expiryDate: new Date('2025-01-01'),
+    image: 'https://www.tigerbrandsfoodservicesolutions.com/-/media/Project/Tiger/OOH/OOH/Products/Koo/Chakalaka-Mild-and-Spicy-With-Beans-410g_web.png?h=494&iar=0&w=350&hash=00D18F86FB683AEBF1FEF5EC3C4198AA',
+  },
+  {
+    id: '4',
+    name: 'Peanut Butter',
+    quantity: 1,
+    category: 'Cupboard',
+    addedAt: new Date('2023-10-01'),
+    updatedAt: new Date('2023-10-01'),
+    expiryDate: new Date('2024-07-01'),
+    image: 'https://www.checkers.co.za/medias/10239475EA-checkers515Wx515H?context=bWFzdGVyfGltYWdlc3wyNTIwMDN8aW1hZ2UvcG5nfGltYWdlcy9oYzUvaDJhLzEwOTk5NDY0NTU4NjIyLnBuZ3wzN2E0MmRhZWM3ODM3MmZhMmVlYWVhMzU4M2MzOGVhOWY1ODRlMDcxZWNiNGQwYTcxMjA5Y2RmZDQwOGFjZTVj',
+  },
+  {
+    id: '5',
+    name: 'Tomato Sauce',
+    quantity: 3,
+    category: 'Cupboard',
+    addedAt: new Date('2023-10-01'),
+    updatedAt: new Date('2023-10-01'),
+    expiryDate: new Date('2024-05-01'),
+    image: 'https://onseshop.co.za/wp-content/uploads/2020/04/ALL-GOLD-SAUCE-350ML-TOMATO.jpeg',
+  },
+  {
+    id: '6',
+    name: 'Sugar',
+    quantity: 2,
+    category: 'Cupboard',
+    addedAt: new Date('2023-10-01'),
+    updatedAt: new Date('2023-10-01'),
+    expiryDate: new Date('2025-10-01'),
+    image: 'https://exclusivelyfood.co.za/wp-content/uploads/2020/03/HULETTS-WHITE-SUGAR-12.5kg.jpg',
+  },
+  {
+    id: '7',
+    name: 'Salt',
+    quantity: 1,
+    category: 'Cupboard',
+    addedAt: new Date('2023-10-01'),
+    updatedAt: new Date('2023-10-01'),
+    expiryDate: new Date('2025-10-01'),
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7Pkn5ajYHd-sMpMt2jyg6ExrhVPQ8qJvlqA&s',
+  },
+  {
+    id: '8',
+    name: 'Cooking Oil',
+    quantity: 2,
+    category: 'Cupboard',
+    addedAt: new Date('2023-10-01'),
+    updatedAt: new Date('2023-10-01'),
+    expiryDate: new Date('2024-10-01'),
+    image: 'https://cdn-prd-02.pnp.co.za/sys-master/images/h61/h86/11400348467230/silo-product-image-v2-10Apr2024-180127-6009667670827-Straight_on-219974-2202_400Wx400H',
+  },
+  {
+    id: '9',
+    name: 'Rice',
+    quantity: 3,
+    category: 'Cupboard',
+    addedAt: new Date('2023-10-01'),
+    updatedAt: new Date('2023-10-01'),
+    expiryDate: new Date('2025-10-01'),
+    image: 'https://www.kitkatgroup.com/Shop/File/StockPart/694?w=401&h=412&quality=70&zoomcrop=0&align=',
+  },
+  {
+    id: '10',
+    name: 'Pasta',
+    quantity: 2,
+    category: 'Cupboard',
+    addedAt: new Date('2023-10-01'),
+    updatedAt: new Date('2023-10-01'),
+    expiryDate: new Date('2025-10-01'),
+    image: 'https://www.shoprite.co.za/medias/10154874EA-inBoxImage-checkers515Wx515H?context=bWFzdGVyfGltYWdlc3wyMzg2MzV8aW1hZ2UvcG5nfGltYWdlcy9oY2EvaGQ1LzEwNTA3NTk0NTk2MzgyLnBuZ3w3NWRhY2I1ZmJiNTA3NjNkYTUyMmRkNmIzZTkzNjk3YTJmZWE5NWY2NTY3ODEwNTI1OTNlZjgwNjYwM2E0NDBl',
+  },
+];
 
-export type Item = {
-  id: string;
-  name: string;
-  quantity: number;
-  location: string;
-  category: Category;
-  addedAt: Date;
-  updatedAt: Date;
-};
-
-let items: Item[] = [];
-
-export function addItem(item: Item): void {
-  item.addedAt = new Date();
-  item.updatedAt = new Date();
-  items.push(item);
-}
-
-export function removeItem(id: string): void {
-  items = items.filter(item => item.id !== id);
-}
-
-export function updateItem(updatedItem: Item): void {
-  updatedItem.updatedAt = new Date();
-  items = items.map(item => (item.id === updatedItem.id ? updatedItem : item));
-}
-
-export function getItems(): Item[] {
-  return items;
-}
-
-export function getItemsByCategory(category: Category): Item[] {
-  return items.filter(item => item.category === category);
-}
+export default items;
