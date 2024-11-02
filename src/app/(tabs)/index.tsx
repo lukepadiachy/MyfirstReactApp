84777ce1ce323417f7f11e7dff7b59229eafea80
🅿️adi <148848073+lukepadiachy@users.noreply.github.com>
2024-11-02 23:41:33 +0200

    Created Custom Component
    - Removed the component I made via a prop which can render different things from one component.
    - Created a tsx file in component
    - removed excess imports

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,56 +1,13 @@
-import Colors from '@/src/constants/Colors';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { View } from 'react-native';
 import items from '@/assets/data/items';
+import ItemList from '@/src/components/ItemList';
 
-const item = items[1]
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-     <Text style={styles.title}>{item.name}</Text>
-      <Image source={{uri: item.image}} style={styles.image} />
-     <Text style={styles.subtitle}>Quantity : {item.quantity}</Text>
-     <Text style={styles.subtitle}>Category : {item.category}</Text>
-     <Text style={styles.subtitle}>Date Added : {item.addedAt.toDateString()}</Text>
-     <Text style={styles.subtitle}>Date Updated : {item.updatedAt.toDateString()}</Text>
-     <Text style={styles.subtitle}>Expiry Date : {item.expiryDate.toDateString()}</Text>
-
-     
+    <View>
+     <ItemList item={items[6]} /> 
+     <ItemList item={items[1]} /> 
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-
-  },
-
-  title: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    marginVertical: 20,
-    color: Colors.light.tint,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  subtitle: {
-    fontSize: 18,
-    color: Colors.light.tint,
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-    borderRadius: 10,
-
-
-  },
-
-
-
-});
---
+++ src/components/ItemList.tsx
@@ -0,0 +1,48 @@
+import Colors from '@/src/constants/Colors';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const ItemList = ({ item }) => {
+
+  return (
+    <View style={styles.container}>
+     <Text style={styles.title}>{item.name}</Text>
+      <Image source={{uri: item.image}} style={styles.image} />
+     <Text style={styles.subtitle}>Quantity : {item.quantity}</Text>
+     <Text style={styles.subtitle}>Category : {item.category}</Text>
+     <Text style={styles.subtitle}>Date Added : {item.addedAt.toDateString()}</Text>
+     <Text style={styles.subtitle}>Date Updated : {item.updatedAt.toDateString()}</Text>
+     <Text style={styles.subtitle}>Expiry Date : {item.expiryDate.toDateString()}</Text>
+    </View>
+
+  );
+};
+
+export default ItemList;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginVertical: 20,
+    color: Colors.light.tint,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  subtitle: {
+    fontSize: 18,
+    color: Colors.light.tint,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+    borderRadius: 10,
+  },
+});
