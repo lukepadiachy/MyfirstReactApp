acc8546eab9aca714f41c809f3d12761a1602188
🅿️adi <148848073+lukepadiachy@users.noreply.github.com>
2024-11-04 20:48:27 +0200

    small screen changes
    Added Stack.Screens

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,4 +1,4 @@
-import { useLocalSearchParams } from "expo-router"; // hook imported from router
+import { Stack, useLocalSearchParams } from "expo-router"; // hook imported from router
 import { View, Text } from "react-native"; // make sure this comes from react and not /../themed
 
 const ItemDetailScreen = () => {
@@ -7,6 +7,8 @@ const ItemDetailScreen = () => {
 
   return (
     <View>
+      <Stack.Screen options={{title: 'Details'}}/>
+
       <Text style={{fontSize: 20}}>ItemDetailScreen for id: {id}</Text>
     </View>
   );
